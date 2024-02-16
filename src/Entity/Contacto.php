16c1faa7c7feedadce24dc4a89de31049df61491
cristian <cristian.nievas@insbaixcamp.cat>
2024-02-16 09:57:12 +0100

    add getters & setters usuario

--- src/Entity/Contacto.php
+++ src/Entity/Contacto.php
@@ -42,6 +42,18 @@ class Contacto
      */
     private $usuario;
 
+    public function getUsuario(): ?Usuario
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario(?Usuario $usuario): self
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
