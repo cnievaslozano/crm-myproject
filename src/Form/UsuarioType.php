5297d11d36cac0e417b4416b354ce61de269ca84
cristian <cristian.nievas@insbaixcamp.cat>
2024-02-16 10:10:57 +0100

    TypeUsuario empresa choice

--- src/Form/UsuarioType.php
+++ src/Form/UsuarioType.php
@@ -10,11 +10,28 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Repository\EmpresaRepository;
 
 class UsuarioType extends AbstractType
 {
+    private $empresaRepository;
+
+    public function __construct(EmpresaRepository $empresaRepository)
+    {
+        $this->empresaRepository = $empresaRepository;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $empresas = $this->empresaRepository->findAll(); // Obtener todas las empresas de la base de datos
+
+        // Transformar los datos de las empresas en el formato necesario para las opciones del desplegable
+        $choices = [];
+        foreach ($empresas as $empresa) {
+            // Utilizar la entidad completa como valor y nombre de opción
+            $choices[$empresa->getNombre()] = $empresa;
+        }
+
         $builder
             ->add('username', TextType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
@@ -32,7 +49,13 @@ class UsuarioType extends AbstractType
             ->add('password', PasswordType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
             ])
-            // ->add('empresa') relacion no lo pilla
+
+            ->add('empresa', ChoiceType::class, [
+                'choices' => $choices,
+                'choice_value' => 'id', // Campo utilizado como valor en el desplegable (puede omitirse si el ID es la clave)
+                'choice_label' => 'nombre', // Campo utilizado como etiqueta en el desplegable
+                'placeholder' => 'Selecciona una empresa', // Opcional: texto para la opción predeterminada
+            ])
             //->add('briefing_web')
             //->add('briefing_app')
             //->add('briefing_logo')
