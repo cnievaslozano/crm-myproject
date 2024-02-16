60ee398442f02fe8632a8205ad05f5ddd9aeff7e
cristian <cristian.nievas@insbaixcamp.cat>
2024-02-16 10:06:29 +0100

    contactotype

---
+++ src/Form/ContactoType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contacto;
+use App\Repository\UsuarioRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+class ContactoType extends AbstractType
+{
+    private $usuarioRepository;
+
+    public function __construct(UsuarioRepository $usuarioRepository)
+    {
+        $this->usuarioRepository = $usuarioRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre_contacto', TextType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+            ])
+            ->add('descripcion_contacto', TextareaType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+            ])
+            ->add('telefono', TextType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+            ])                    
+            ->add('usuario', ChoiceType::class, [
+                'choices' => $this->getUsuariosSinRolAdmin(),
+                'choice_label' => 'username', 
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Crear Contacto',
+                'attr' => ['class' => 'btn custom-btn btn-lg btn-block'],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contacto::class,
+        ]);
+    }
+
+    private function getUsuariosSinRolAdmin(): array
+    {
+        $usuarios = $this->usuarioRepository->findUsuariosSinRolAdmin();
+
+        $choices = [];
+        foreach ($usuarios as $usuario) {
+            $choices[$usuario->getUsername()] = $usuario;
+        }
+
+        return $choices;
+    }
+}
