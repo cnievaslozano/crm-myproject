490bab7b8fb4cdde752f1813b14ca58e09ee7bfd
cristian <cristian.nievas@insbaixcamp.cat>
2024-02-22 10:04:33 +0100

    function new

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,12 +4,59 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface; 
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UsuarioType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 class UserController extends AbstractController
 {
     public function index(): Response
     {
         return $this->render('login/index.html.twig');
+    }
+
+    public function new(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response 
+    {
+        $user = new Usuario();
+        $form = $this->createForm(UsuarioType::class, $user);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $user = $form->getData();
+            $textoplanoPassword = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $textoplanoPassword
+            );
+            $user->setPassword($hashedPassword);
+    
+            // Verificar si el ID del usuario es 1 para asignar el rol "ADMIN"
+            $user->setRoles($user->getId() === 1 ? ['ROLE_ADMIN'] : ['ROLE_USER']);
+            $user->setFechaCreacionUsuario(new \DateTime());
+            $user->setActivo(true);
+    
+            try {
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('success', 'Se ha registrado correctamente.');
+                return $this->redirectToRoute('registro_usuario');
+            } catch (UniqueConstraintViolationException $e) {
+                $this->addFlash('error', 'El nombre de usuario ya está en uso. Por favor, elige otro.');
+            } catch (\Exception $e) {
+                // Otra excepción no esperada
+                $this->addFlash('error', 'Ha ocurrido un error al registrar el usuario.');
+                // Puedes agregar un registro de error detallado aquí si es necesario
+                // return $this->redirectToRoute('ruta_donde_quieres_redirigir');
+            }
+        }
+    
+        return $this->render('formularios/usuario.html.twig', [
+            'nombre_empresa' => 'ejemplo',
+            'controller_name' => 'RegistroController',
+            'form' => $form->createView(),
+        ]);
     }
 }
