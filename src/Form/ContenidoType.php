97841684928879b8a051863cb9c978b3ff519ed7
cristian <cristian.nievas@insbaixcamp.cat>
2024-02-22 09:02:20 +0100

    field ruta_imagenes_contenidos

--- src/Form/ContenidoType.php
+++ src/Form/ContenidoType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ContenidoType extends AbstractType
 {
@@ -28,11 +29,16 @@ class ContenidoType extends AbstractType
             ->add('contenido', TextareaType::class, [
                 'attr' => [
                     'class' => 'form-control form-control-lg',
-                    'style' => 'height: 400px',
+                    'style' => 'height: 500px',
                 ],
             ])
             /*temporalmente sera un campo texto */
-            ->add('ruta_imagenes')
+            ->add('ruta_imagenes_contenidos', FileType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-lg',
+                    'required' => false,
+                ],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enviar Contenido',
                 'attr' => ['class' => 'btn custom-btn btn-lg btn-block'],
