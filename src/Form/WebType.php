dd50e62517b142d5b1c11c5030ad0228bbec69a3
cristian <cristian.nievas@insbaixcamp.cat>
2024-03-05 10:06:05 +0100

    WebType

---
+++ src/Form/WebType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Web;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class WebType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('web', TextType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, ingresa una respuesta.'
+                    ]),
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
+            ])
+            ->add('descripcion_web', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-lg',
+                    'style' => 'height: 150px',
+                    
+                ],
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'max' => 150,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Añadir web',
+                'attr' => ['class' => 'btn btn-granota btn-lg btn-block'],
+            ]);;;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Web::class,
+        ]);
+    }
+}
