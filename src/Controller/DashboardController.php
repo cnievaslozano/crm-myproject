78c1ae7e4d21f6c6a75d72716776fdf1fd0c6c4e
cristian <cristian.nievas@insbaixcamp.cat>
2024-03-05 10:02:33 +0100

    real data for index

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -11,13 +11,38 @@ use App\Repository\ContenidoRepository;
 use App\Repository\IncidenciaRepository;
 use App\Repository\EmpresaRepository;
 
+
 class DashboardController extends AbstractController
 {
-    
 
-    public function index(): Response 
-    {
-        return $this->render('dashboard/index.html.twig');
+
+    public function index(
+        BriefingWebRepository $briefingWebRepository,
+        BriefingLogoRepository $briefingLogoRepository,
+        BriefingAppRepository $briefingAppRepository,
+        ContenidoRepository $contenidoRepository,
+        IncidenciaRepository $incidenciaRepository,
+        EmpresaRepository $empresaRepository
+    ): Response {
+
+        // Obtener los últimos 4 briefings de cada tipo
+        $ultimosBriefingsWeb = $briefingWebRepository->findLastN(4);
+        $ultimosBriefingsLogo = $briefingLogoRepository->findLastN(4);
+        $ultimosBriefingsApp = $briefingAppRepository->findLastN(4);
+
+        // Obtener los últimos clientes, incidencias y contenidos
+        $ultimosClientes = $empresaRepository->findLastN(4);
+        $ultimasIncidencias = $incidenciaRepository->findLastN(4);
+        $ultimosContenidos = $contenidoRepository->findLastN(4);
+
+        return $this->render('dashboard/index.html.twig', [
+            'ultimosbriefingsWeb' => $ultimosBriefingsWeb,
+            'ultimosbriefingsLogo' => $ultimosBriefingsLogo,
+            'ultimosbriefingsApp' => $ultimosBriefingsApp,
+            'ultimosclientes' => $ultimosClientes,
+            'ultimasincidencias' => $ultimasIncidencias,
+            'ultimoscontenidos' => $ultimosContenidos,
+        ]);
     }
 
     public function clientes(EmpresaRepository $empresaRepository): Response
@@ -28,7 +53,7 @@ class DashboardController extends AbstractController
     }
 
 
-    public function briefings(BriefingAppRepository $briefingAppRepository,BriefingWebRepository $briefingWebRepository, BriefingLogoRepository $briefingLogoRepository): Response
+    public function briefings(BriefingAppRepository $briefingAppRepository, BriefingWebRepository $briefingWebRepository, BriefingLogoRepository $briefingLogoRepository): Response
     {
 
 
@@ -39,19 +64,18 @@ class DashboardController extends AbstractController
         $briefings = array_merge($briefingApps, $briefingWebs, $briefingLogos);
 
         // ordena por fecha
-        usort($briefings, function($a, $b) {
+        usort($briefings, function ($a, $b) {
             return $a->getFecha()->getTimestamp() - $b->getFecha()->getTimestamp();
         });
 
         return $this->render('dashboard/briefings.html.twig', [
             'briefings' => $briefings,
         ]);
-        
     }
 
     public function contenidos(ContenidoRepository $contenidoRepository): Response
     {
-        
+
         $contenidos = $contenidoRepository->findAllWithBriefingWebEmpresaAndUser();
 
         return $this->render('dashboard/contenidos.html.twig', [
@@ -59,7 +83,7 @@ class DashboardController extends AbstractController
         ]);
     }
 
-    
+
     public function incidencias(IncidenciaRepository $incidenciaRepository): Response
     {
 
@@ -69,5 +93,4 @@ class DashboardController extends AbstractController
             'incidencias' => $incidencias,
         ]);
     }
-
 }
