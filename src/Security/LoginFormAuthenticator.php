41797f3513f4846f08123a6a3e04ac9d15845ecd
cristian <cristian.nievas@insbaixcamp.cat>
2024-03-13 10:41:03 +0100

    Validate user is active

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -16,25 +16,31 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
-
+    private $userProvider;
     public const LOGIN_ROUTE = 'login';
 
     private $urlGenerator;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    public function __construct(UrlGeneratorInterface $urlGenerator, UserProviderInterface $userProvider)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->userProvider = $userProvider;
     }
 
     public function authenticate(Request $request): Passport
     {
         $username = $request->request->get('username', '');
+        $user = $this->userProvider->loadUserByIdentifier($username);
 
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
+        if (!$user || !$user->isActivo()) {
+            throw new CustomUserMessageAuthenticationException('Tu cuenta no está activa. Contacta con un administrador');
+        }
 
         return new Passport(
             new UserBadge($username),
@@ -56,13 +62,13 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         $user = $token->getUser();
         if ($user) {
             $rol = $user->getRoles();
-            if ($rol[0] == 'ROLE_USER'){
+            if ($rol[0] == 'ROLE_USER') {
                 return new RedirectResponse('inicio');
             } else {
                 return new RedirectResponse('dashboard');
             }
         }
-        
+
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
