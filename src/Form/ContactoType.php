3c24cb610a201d8fcf4c8b7cdc5ef102129ebaf5
cristian <cristian.nievas@insbaixcamp.cat>
2024-03-20 09:05:43 +0100

    Validate fields form

--- src/Form/ContactoType.php
+++ src/Form/ContactoType.php
@@ -12,7 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ContactoType extends AbstractType
 {
@@ -28,22 +29,51 @@ class ContactoType extends AbstractType
         $builder
             ->add('nombre_contacto', TextType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, ingresa una respuesta.'
+                    ]),
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
             ])
             ->add('descripcion_contacto', TextareaType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
             ])
             ->add('email', EmailType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
+                'required' => false, 
+                'constraints' => [
+                    new Length([
+                        'max' => 150,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
             ])
             ->add('telefono', TextType::class, [
                 'attr' => ['class' => 'form-control form-control-lg'],
-            ])                    
+                'required' => false, 
+                'constraints' => [
+                    new Length([
+                        'max' => 9,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ]
+            ])
 
             ->add('submit', SubmitType::class, [
                 'label' => 'Crear Contacto',
                 'attr' => ['class' => 'btn btn-granota btn-lg btn-block'],
-            ]);
-        ;
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -52,6 +82,4 @@ class ContactoType extends AbstractType
             'data_class' => Contacto::class,
         ]);
     }
-
-
 }
