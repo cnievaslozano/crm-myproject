f6775d83002b58aa3c21f3561c672ca81581ce58
cristian <cristian.nievas@insbaixcamp.cat>
2024-04-03 10:57:20 +0200

    Form ForgotPassword

---
+++ src/Form/ForgotPasswordType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ForgotPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('usuario', TextType::class, [
+                'attr' => ['class' => 'form-control form-control-lg'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, ingresa una respuesta.'
+                    ]),
+                    new Length([
+                        'max' => 150,
+                        'maxMessage' => 'Este campo no puede tener más de {{ limit }} caracteres.'
+                    ])
+                ] 
+
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enviar soicitud',
+                'attr' => ['class' => 'btn btn-granota btn-lg btn-block'],
+            ]);
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
