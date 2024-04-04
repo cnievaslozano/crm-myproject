9ef79213adda9bfa913dd6b17cfdb978263f104b
cristian <cristian.nievas@insbaixcamp.cat>
2024-04-04 12:00:52 +0200

    Form ResetPassrodType

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Las contraseñas deben coincidir.',
+                
+                'required' => true,
+                'first_options'  => [
+                    'attr' => ['class' => 'form-control form-control-lg mb-4', 'id' => 'password'],
+                    'label' => 'Contraseña Nueva',
+                    'label_attr' => ['class' => 'form-label'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Por favor, introduce una contraseña.',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'La contraseña debe tener al menos {{ limit }} caracteres.',
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'attr' => ['class' => 'form-control form-control-lg', 'id' => 'confirm_password'],
+                    'label' => 'Confirmar Contraseña',
+                    'label_attr' => ['class' => 'form-label'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Por favor, repite la contraseña.',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'La contraseña debe tener al menos {{ limit }} caracteres.',
+                        ]),
+                        new EqualTo([
+                            'propertyPath' => 'password',
+                            'message' => 'Las contraseñas deben coincidir.',
+                        ]),
+                    ],
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Restablecer contraseña',
+                'attr' => ['class' => 'btn btn-granota btn-lg btn-block'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
