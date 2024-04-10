057afc1baa0673cdb6a539167123265e967a50fa
cristian <cristian.nievas@insbaixcamp.cat>
2024-04-10 10:36:02 +0200

    error 404

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionController extends AbstractController
 {
-
-    public function show404(): Response
+    public function showGeneralError(\Throwable $exception): Response
     {
-        return $this->render('404.html.twig', []);
+        // Comprobamos si es un error 404
+        if ($exception instanceof HttpExceptionInterface) {
+            
+            if ($exception->getStatusCode() == 404) {
+                return $this->render('404.html.twig', [], new Response('', 404));
+            }
+        }
 
+        // Si no es una excepción HTTP o es un error 404, deja que Symfony maneje el error
+        throw $exception;
     }
 }
